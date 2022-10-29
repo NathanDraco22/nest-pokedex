066d023ef13ea143d19395fdfc0ce225adad155d
NathanDraco22 <nathandraco22@gmail.com>
2022-10-28 20:59:01 -0600

    seed implemented

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { JSONResponse, Result } from './interfaces/pokemon-response.interface';
 
 @Injectable()
 export class SeedService {
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel : Model<Pokemon>
+  ){}
+
   async executeSEED(){
 
-    const result = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    await this.pokemonModel.deleteMany();
+
+    const result = await fetch("https://pokeapi.co/api/v2/pokemon?limit=650");
     const json : JSONResponse = await result.json();
     const responseArray : Result[] = json.results;
 
@@ -16,6 +26,8 @@ export class SeedService {
       const pkmNumber = +array[array.length-2];
       pokemonData.push({name : value.name , no : pkmNumber})
     });
+
+    await this.pokemonModel.insertMany(pokemonData);
 
     return "SEED executed";
   }
